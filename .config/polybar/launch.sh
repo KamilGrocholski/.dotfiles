6ffcdab0cbf09896050e894960da36bd246e27bc
Kamil Grocholski <kamil.sebastian.grocholski@gmail.com>
2023-07-08 14:09:52 +0200

    feat: polybar, i3-gaps

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+THEME="mikalsqwe"
+
+# Terminate already running bar instances
+killall -q i3bar
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+CONFIG_DIR=~/.config/polybar/$THEME/config.ini
+polybar main -c $CONFIG_DIR &
+echo "Polybar launched..."
